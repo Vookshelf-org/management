04d539733e1b2c3345ef49e6e94ede300c4d8208
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-25 03:55:52 +0900

    🔧 Storybookに関連する設定

---
+++ .storybook/main.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  stories: ['../stories/**/*.stories.(ts|tsx|js|jsx|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+        },
+      },
+    },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+}
