db7b9b06278c9be337f6bece5c45ab6ba92754f8
SnO2WMaN <sno2wman.official@gmail.com>
2020-03-02 00:51:34 +0900

    🔧(Next.js) next-imagesへの対応

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const path = require('path')
 
+const withImages = require('next-images')
+
 const rootDir = path.resolve(__dirname)
 const srcDir = path.resolve(rootDir, 'src')
 
-module.exports = {
+module.exports = withImages({
   webpack: config => {
     /* eslint-disable no-param-reassign */
     config.resolve.alias['~'] = srcDir
@@ -11,4 +13,4 @@ module.exports = {
     return config
     /* eslint-enable no-param-reassign */
   },
-}
+})
