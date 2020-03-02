58924456028ae67aae4887f3c021d98480730ae6
SnO2WMaN <sno2wman.official@gmail.com>
2020-03-02 20:34:27 +0900

    ✨ 別名義を作者ページに表示

--- src/pages/author/[id].tsx
+++ src/pages/author/[id].tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import Book from '~/components/BooksList/Covers/Book'
 import { getAuthor } from '~/queries/authors'
@@ -12,6 +13,7 @@ export type Props = { className?: string }
 
 const Page: NextPage<Props> = ({ className }) => {
   const router = useRouter()
+  const { t } = useTranslation()
   const { id } = router.query
 
   const { data } = useQuery<
@@ -40,10 +42,34 @@ const Page: NextPage<Props> = ({ className }) => {
   return (
     <main className={classnames(className)}>
       <div className={classnames('container', 'mx-auto', 'py-6')}>
-        <h1 className={classnames('text-2xl', 'font-bold')}>
+        <h1
+          className={classnames('text-4xl', 'font-bold', 'text-black', 'mb-4')}
+        >
           {data?.author.name}
         </h1>
-        <BooksListTable data={listData} className={classnames('w-full')} />
+        <div
+          className={classnames(
+            'container',
+            'mx-auto',
+            'mb-4',
+            'flex',
+            'items-center'
+          )}
+        >
+          <span className={classnames('mr-2', 'text-gray-600')}>
+            {t('common:alias')}
+          </span>
+          <p className={classnames('text-gray-600')}>
+            {data?.author.aliases.map(({ name }, i, { length }) => (
+              <span key={i}>
+                <span className={classnames('text-lg', 'text-black')}>
+                  {name}
+                </span>
+                {i === length && ','}
+              </span>
+            ))}
+          </p>
+        </div>
         <div
           className={classnames(
             'grid',
